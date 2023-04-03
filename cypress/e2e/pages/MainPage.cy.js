b2b17cb5df45aad409aeaa4d0307e716c0164fe7
asm <vaskocuturilo@rambler.ru>
2023-04-03 13:09:09 +0200

    Fix: fix page object pattern

--- cypress/e2e/pages/MainPage.cy.js
+++ cypress/e2e/pages/MainPage.cy.js
@@ -1,15 +1,33 @@
 class MainPage {
-    visit() {
-        cy.visit("/")
-      }
 
-    checkElements(){
-        cy.title().should('eq', 'Bankist')
-        cy.get('[placeholder="user"]').should('be.visible');
-        cy.get('[placeholder="PIN"]').should('be.visible');
-        cy.get('[class="login__btn"]').should('be.visible');
-       
-    }
+ elements = {
+
+    title : () => cy.title(),
+    userInput: () => cy.get('[placeholder="user"]'),
+    passwordInput : () => cy.get('[placeholder="PIN"]').should('be.visible'),
+    loginButton: () => cy.get('[class="login__btn"]').should('be.visible'),
+    welcomeText : () => cy.get('[class="welcome"]'),
+    balance : () => cy.get('.balance__value')  
+} 
+
+visit() {
+  cy.visit("/")
+}
+
+checkAllElements() {
+    this.elements.title().should('eq', 'Bankist');
+    this.elements.userInput().should('be.visible');
+    this.elements.passwordInput().should('be.visible');
+    this.elements.loginButton().should('be.visible');
+ }
+
+ checkWelcomeMessage(name) { 
+    return this.elements.welcomeText().should('contain', `Welcome back, ${name}`)
+}
+
+checkUserBalance(value) {
+  return this.elements.balance().should('be.visible').contains(value)
+}
 }
 
 export default MainPage;
